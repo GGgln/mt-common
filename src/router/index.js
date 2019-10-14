06932309831157d7b778484561eabc40cc631be4
PC-20190315DLDZ\Administrator <18630840206@163.com>
2019-10-14 15:17:35 +0800

    提交人 吴伯莹

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,11 @@ import alarm from '@/components/alarm'
 import log from '@/components/log'
 import consumer from '@/components/consumer'
 import param from '@/components/param'
-<<<<<<< HEAD
+// <<<<<<< HEAD
 import login from '@/components/login'
-=======
+// =======
 import _class from '@/components/class'
->>>>>>> 8b0d576906d8a233108430a1f96cb2490864ee8f
+// >>>>>>> 8b0d576906d8a233108430a1f96cb2490864ee8f
 Vue.use(Router)
 
 export default new Router({
@@ -34,15 +34,17 @@ export default new Router({
       component: param
     },
     {
-<<<<<<< HEAD
+// <<<<<<< HEAD
       path: '/login',
       name: 'login',
       component: login
-=======
+      },
+      {
+// =======
       path: '/class',
       name: 'class',
       component: _class
->>>>>>> 8b0d576906d8a233108430a1f96cb2490864ee8f
+// >>>>>>> 8b0d576906d8a233108430a1f96cb2490864ee8f
     }
   ]
 })
